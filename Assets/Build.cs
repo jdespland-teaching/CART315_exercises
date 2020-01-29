b88e280cc25d1094ab9d6bf159ed9fa8e2033cdd
jdespland-teaching <46430973+jdespland-teaching@users.noreply.github.com>
2020-01-29 13:55:44 -0500

    Week 4 Exercise

---
+++ Assets/Build.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Build : MonoBehaviour
+{
+    public GameObject thecamera;
+    public GameObject create;
+
+    private int count = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        count = count + 1;
+        if (count >= 3) count = 0;
+        if (Input.GetMouseButton(1) && count == 0)
+        {
+            RaycastHit hit;
+            Ray ray = thecamera.GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
+                GameObject.Instantiate(create, hit.point, Quaternion.identity);
+            }
+        }
+    }
+}
